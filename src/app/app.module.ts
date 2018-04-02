e29165bdf5dade9fd2067335151b3d64a8dd9f2e
Burouc <the@burouc.com>
2018-04-02 20:26:16 +0200

    Add bootstrap and trade-view module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TradeViewModule } from './trade-view/trade-view.module';
 
 
 @NgModule({
@@ -10,9 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TradeViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
